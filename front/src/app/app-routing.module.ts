af5de370aadad7f026c380c7ac6a6337365ba2ac
Alexander Olmedo <olmedo2408@gmail.com>
2019-07-31 17:43:54 -0500

    Nav

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RegistroComponent} from './registro/registro.component';
+import {BusquedaComponent} from './busqueda/busqueda.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'registro', component: RegistroComponent},
+  {path: 'busqueda', component: BusquedaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistroComponent } from './registro/registro.component';
 import { BusquedaComponent } from './busqueda/busqueda.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenu, MatMenuModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { BusquedaComponent } from './busqueda/busqueda.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
